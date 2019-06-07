ab616de440139a8cf0800b82e0b8568a40f83702
jverger <julien.verger@accor.com>
2019-06-07 11:34:30 +0200

    Change initial 'Hello World' to something more funny

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hell fest is coming !!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
